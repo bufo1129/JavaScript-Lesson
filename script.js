d3ddc1abc5f2d04595acae1fa8e655e9ca5f071f
Bufo <124179824+bufo1129@users.noreply.github.com>
2023-04-09 02:14:39 +0000

    [Add]天気追加

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ $(function() {
     event.preventDefault();
   });
 });
+
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
+});
+
+// <!--ハンバーガーメニューとタブメニューでそれぞれのJSを入れるときはどうやって書くの？？？-->
+// function内に書いてもダメ、外に新しくfunction作ってもダメだった
+// ファイルを別で作成して読み込む？？
